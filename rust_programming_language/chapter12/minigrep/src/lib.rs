e298c1da62db506471083112c8ec862d9a699f66
chengzw <chengzw258@163.com>
2023-09-22 22:22:55 +0800

    改进之前的 I/O 项目

--- rust_programming_language/chapter12/minigrep/src/lib.rs
+++ rust_programming_language/chapter12/minigrep/src/lib.rs
@@ -24,13 +24,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
         let ignore_case= env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
@@ -42,14 +47,17 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    // let mut results = Vec::new();
+    //
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
+    // results
+    contents.lines()
+        .filter(|line|line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
